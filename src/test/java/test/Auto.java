ead4b6dcc521b5494ad2436ce6a6a543881fde72
SantiagoOchoaQuintero <sochoaqu@unal.edu.co>
2024-02-17 22:43:51 -0500

    Update Auto.java
    Intento solucion error de metodos en clase auto

--- src/test/java/test/Auto.java
+++ src/test/java/test/Auto.java
@@ -5,15 +5,14 @@ public class Auto {
     String marca;
     Motor motor;
     int registro;
-    Asiento asiento;
-    int cantidadCreados;
+    static int cantidadCreados;
     Asiento[] asientos;
     int x = 0;
 
     public int cantidadAsientos(){
         int cantidadSillas = 0;
-        for(int g = 0; g<=asientos.length;g++){
-            if(asientos[g] instanceof Object){
+        for(int g = 0; g<asientos.length;g++){
+            if(asientos[g] instanceof Asiento){
                 cantidadSillas++;
             }
         }
@@ -22,7 +21,7 @@ public class Auto {
     public String verificarIntegridad(){
         String y = "";
         if(this.motor.registro==this.registro){
-            for(int i = 0; i<=asientos.length ; i++){
+            for(int i = 0; i<asientos.length ; i++){
                 if(asientos[i]!=null){
                     if (asientos[i].registro==this.registro) {
                         y = "Auto original";
